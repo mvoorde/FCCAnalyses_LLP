d90de70e9763d5c453148079ab98b45fb69d23cd
mvoorde <m.voorde96@gmail.com>
2023-04-18 19:15:03 +0200

    updating DV code wrt to changes in PR 247

--- analyzers/dataframe/src/VertexingUtils.cc
+++ analyzers/dataframe/src/VertexingUtils.cc
@@ -1,5 +1,6 @@
 #include "FCCAnalyses/VertexingUtils.h"
 #include "FCCAnalyses/VertexFitterSimple.h"
+#include "FCCAnalyses/myUtils.h"
 
 #include "TrkUtil.h" // from delphes
 
@@ -18,6 +19,111 @@ TVectorD XPtoPar(TVector3 x, TVector3 p, Double_t Q) {
   TrkUtil tu;
   return tu.XPtoPar(x, p, Q, fB);
 }
+
+// ----------------------------------------------------------------------------------------------------------------------
+// ---- Following code block is used in the Exotic Higgs Decays to LLPs analysis to find DVs with the SV finder of LCFI+
+// ----
+
+//
+// function to merge vertices if the distance between them are less than 10*error on the position, or if they are within 1 mm from each other
+//
+ROOT::VecOps::RVec<FCCAnalysesVertex> mergeVertices ( ROOT::VecOps::RVec<FCCAnalysesVertex> vertices_in, const ROOT::VecOps::RVec<edm4hep::TrackState>& alltracks ) {
+  ROOT::VecOps::RVec<FCCAnalysesVertex> result;
+  ROOT::VecOps::RVec<FCCAnalysesVertex> merged_vertices;
+  int n = vertices_in.size();
+
+  // only try merging if there are 2 or more vertices
+  if (n == 0 || n == 1) {
+    result = vertices_in;
+    return result;
+  }
+
+  // check distance between vertices pair-wise
+  for (int i=0; i < n-1; i++){
+    FCCAnalysesVertex ivtx = vertices_in.at(i);
+
+    for (int j=i+1; j < n; j++){
+      FCCAnalysesVertex jvtx = vertices_in.at(j);
+      float distance = FCCAnalyses::myUtils::get_distanceVertex(ivtx.vertex, jvtx.vertex, -1);   //input on edm4::vertexdata format, int comp = -1 to get distance in xyz
+      float error = FCCAnalyses::myUtils::get_distanceErrorVertex(ivtx.vertex, jvtx.vertex, -1);
+      float threshold = 10*error;
+
+      if (distance < threshold || distance < 1){
+        ROOT::VecOps::RVec<edm4hep::TrackState> tracks;
+        ROOT::VecOps::RVec<int> tracks_indices_i = ivtx.reco_ind;
+        ROOT::VecOps::RVec<int> tracks_indices_j = jvtx.reco_ind;
+
+        for (int k = 0; k < tracks_indices_i.size(); k++) {
+          tracks.push_back(alltracks[tracks_indices_i.at(k)]);
+        }
+
+         for (int k = 0; k < tracks_indices_j.size(); k++) {
+          tracks.push_back(alltracks[tracks_indices_j.at(k)]);
+        }
+
+        // get the associated tracks to each vertex, combine and do a refit
+        FCCAnalysesVertex mergedVertex = VertexFitterSimple::VertexFitter_Tk(2, tracks, alltracks);
+
+        merged_vertices.push_back(mergedVertex);
+
+        for (int k = 0; k < n; k++) {
+          if (k != i && k != j) {
+            merged_vertices.push_back(vertices_in.at(k));
+          }
+        }
+
+        //recursive process, repeat until vertices that all have distance > threshold are found
+        result = FCCAnalyses::VertexingUtils::mergeVertices(merged_vertices);
+
+        return result;
+      }
+    }
+
+    result.push_back(ivtx);
+  }
+  result.push_back(vertices_in.at(n-1));
+  return result;
+}
+
+//
+// selection of tracks based on the transverse momentum pT
+//
+sel_pt_tracks::sel_pt_tracks(float arg_min_pt) : m_min_pt(arg_min_pt) {};
+ROOT::VecOps::RVec<edm4hep::TrackState> sel_pt_tracks::operator() (ROOT::VecOps::RVec<edm4hep::TrackState> in) {
+    ROOT::VecOps::RVec<edm4hep::TrackState> result;
+    result.reserve(in.size());
+    for (size_t i = 0; i < in.size(); ++i) {
+        auto & track = in[i];
+        TVectorD track_param = FCCAnalyses::VertexingUtils::get_trackParam(track);  //track parameters in edm4hep format
+        TVector3 tracks_p = FCCAnalyses::VertexingUtils::ParToP(track_param);   //get the momentum of the tracks from the track_parameters
+        double tracks_pt = tracks_p.Pt();   //Pt: the transverse component
+        if (tracks_pt > m_min_pt) {
+            result.emplace_back(track);
+        }
+    }
+    return result;
+}
+
+//
+// selection of tracks based on the impact paramter d0
+//
+sel_d0_tracks::sel_d0_tracks(float arg_min_d0) : m_min_d0(arg_min_d0) {};
+ROOT::VecOps::RVec<edm4hep::TrackState> sel_d0_tracks::operator() (ROOT::VecOps::RVec<edm4hep::TrackState> in) {
+    ROOT::VecOps::RVec<edm4hep::TrackState> result;
+    result.reserve(in.size());
+    for (size_t i = 0; i < in.size(); ++i) {
+        auto & track = in[i];
+        double tr_d0 = fabs(track.D0);
+        if (tr_d0 > m_min_d0) {
+            result.emplace_back(track);
+        }
+    }
+    return result;
+}
+
+// ----
+// ---- end of DV specific code
+// --------------------------------------------------------------------------------------------------------------
 
 //
 // Selection of particles based on the d0 / z0 significances of the associated
